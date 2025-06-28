f85a3abb74b3a7aa953478fcd46f148f25da93c9
Vamshi Thallapalli <127075912+vamshithallapalli@users.noreply.github.com>
2025-06-28 17:21:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 //your JS code here. If required.
+
+window.onload = function(){
+	let savedusername = localStorage.getItem("username");
+	let savedpassword = localStorage.getItem("password");
+
+	if(savedusername && savedpassword){
+		document.getElementById('existing').style.display = 'inline-block';
+	}
+}
+
+document.getElementById("submit").addEventListener('clikc', function(){
+	let username = document.getElementById("username").value;
+	let password = document.getElementById("password").value;
+	let rememberMe = document.getElementById("checkbox").checked;
+
+	alert(`Logged in as ${username}`);
+
+	if(rememberMe){
+		localStorage.setItem('username', username);
+		localStorage.setItem('password', password);
+		document.getElementById('existing').style.display = 'inline-block';
+	}
+	else{
+		localStorage.removeItem('username');
+        localStorage.removeItem('password');
+        document.getElementById('existing').style.display = 'none';
+	}
+	
+})
+document.getElementById('existing').addEventListener('click', function() {
+      let savedUsername = localStorage.getItem('username');
+      if (savedUsername) {
+        alert(`Logged in as ${savedUsername}`);
+      }
+    });
